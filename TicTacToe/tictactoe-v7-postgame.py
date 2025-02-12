4181bb3fc62835104310f493307d59b37cff1b4e
ryanbuck182@ <ryanbuck182@gmail.com>
2025-02-12 01:14:27 -0500

    Remove 3 characters 490->487

--- TicTacToe/tictactoe-v7-postgame.py
+++ TicTacToe/tictactoe-v7-postgame.py
@@ -1,4 +1,4 @@
-import os;p=print;d="\n--+---+--\n";D=" | ".join;u=0;b=list(map(str,range(1,10)));l=lambda x,y,z=1:b[x:y:z].count(b[x])==3
+import os;p=print;d="\n--+---+--\n";D=" | ".join;u=0;b=list(map(str,range(1,10)));l=lambda x,y,z=1:b[x:y:z].count(u)==3
 while 1:
 	p(D(b[:3])+d+D(b[3:6])+d+D(b[6:]))
 	if l(0,3)|l(3,6)|l(6,9)|l(0,9,3)|l(1,9,3)|l(2,9,3)|l(0,9,4)|l(2,9,2):break
