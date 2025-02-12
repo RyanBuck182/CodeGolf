59207bb487429ebff800baa949e4f57f1dc23cc7
ryanbuck182@ <ryanbuck182@gmail.com>
2025-02-12 01:10:58 -0500

    Shorten by 3 characters

--- TicTacToe/tictactoe-v7-postgame.py
+++ TicTacToe/tictactoe-v7-postgame.py
@@ -2,11 +2,11 @@ import os;p=print;d="\n--+---+--\n";D=" | ".join;u=0;b=list(map(str,range(1,10))
 while 1:
 	p(D(b[:3])+d+D(b[3:6])+d+D(b[6:]))
 	if l(0,3)|l(3,6)|l(6,9)|l(0,9,3)|l(1,9,3)|l(2,9,3)|l(0,9,4)|l(2,9,2):break
-	elif len({*b})==2:u='d';break
+	elif len({*b})==2:u=0;break
 	u='o'if u=='x'else'x';m=0
 	while 1:
 		m=input(u+" Pick a move (1-9): ")
 		if m.isdigit()and m in b:break
 		else:p("Invalid move!")
 	b[int(m)-1]=u;os.system('cls')
-p("It's a draw!")if u=='d'else p(u,"wins!")
+p("It's a draw!")if u==0 else p(u,"wins!")
